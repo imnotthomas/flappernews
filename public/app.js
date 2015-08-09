85523cf6dd58893103db04e4c9fcd95a7a655edd
Thomas Altman <thomasjaltman@gmail.com>
2015-08-09 12:58:31 +0000

    adding links

--- public/app.js
+++ public/app.js
@@ -13,9 +13,15 @@ app.controller('MainCtrl', [
 	];
 
 	$scope.addPost = function() {
-	    if(!scope.title || $scope.title === '') {return; }
-	    $scope.posts.push({title: $scope.title, upvotes: 0});
+	    if(!$scope.title || $scope.title === '') {return; }
+	    $scope.posts.push({
+		title: $scope.title,
+		link: $scope.link,
+		upvotes: 0
+	    });
+
 	    $scope.title = '';
+	    $scope.link = '';
 	}
 
 	$scope.incrementUpvotes = function(post) {
