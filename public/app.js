2e491df0d1a03c58d060bb8bab5c815e9a612013
Thomas Altman <thomasjaltman@gmail.com>
2015-08-15 22:50:09 +0000

    added comments

--- public/app.js
+++ public/app.js
@@ -27,11 +27,40 @@ app.controller('MainCtrl', [
 	    $scope.posts.push({
 		title: $scope.title,
 		link: $scope.link,
-		upvotes: 0
+		upvotes: 0,
+		comments: [
+		    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+		    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+		]
 	    });
 
 	    $scope.title = '';
 	    $scope.link = '';
+	}
+
+	$scope.incrementUpvotes = function(post) {
+	    post.upvotes += 1;
+	}
+    }
+]);
+
+app.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts){
+	console.log($stateParams);
+	$scope.post = posts.posts[$stateParams.id];
+
+	$scope.addComment = function() {
+	    if($scope.body === '') {return; }
+	    $scope.post.comments.push({
+		body: $scope.body,
+		author: 'user',
+		upvotes: 0
+	    });
+
+	    $scope.body = '';
 	}
 
 	$scope.incrementUpvotes = function(post) {
@@ -49,6 +78,11 @@ app.config([
 		url: '/home',
 		templateUrl: '/home.html',
 		controller: 'MainCtrl'
+	    })
+	    .state('posts', {
+		 url: '/posts/{id}'
+		,templateUrl: '/posts.html'
+		,controller: 'PostsCtrl'
 	    });
 
 	$urlRouterProvider.otherwise('home');
