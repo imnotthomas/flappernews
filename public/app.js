34de119c29de72743b95fd3b0247c4e76aec1c5f
Thomas Altman <thomasjaltman@gmail.com>
2015-08-15 21:47:22 +0000

    Added posts factory

--- public/app.js
+++ public/app.js
@@ -1,16 +1,26 @@
 var app = angular.module('flapperNews', []);
 
-app.controller('MainCtrl', [
-    '$scope',
-    function($scope){
-	$scope.test = 'Hello world!';
-	$scope.posts = [
+app.factory('posts', [function() {
+    // service body
+    var o = {
+	posts: [
 	    {title: 'post 1', upvotes: 5},
 	    {title: 'post 2', upvotes: 2},
 	    {title: 'post 3', upvotes: 15},
 	    {title: 'post 4', upvotes: 9},
-	    {title: 'post 5', upvotes: 4}
-	];
+	    {title: 'post 5', upvotes: 4},
+	    {title: 'post 6', upvotes: 1}
+	]
+    };
+    return o;
+}])
+
+app.controller('MainCtrl', [
+    '$scope',
+    'posts',
+    function($scope, posts){
+	$scope.test = 'Hello world!';
+	$scope.posts = posts.posts;
 
 	$scope.addPost = function() {
 	    if(!$scope.title || $scope.title === '') {return; }
