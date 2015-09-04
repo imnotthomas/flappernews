7fc0865d23b7380c509c5ff3579c3e44cab572f3
Thomas Altman <thomasjaltman@gmail.com>
2015-09-04 20:51:24 +0000

    adding posts

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -11,5 +11,9 @@ app.controller('MainCtrl', [
 	    {title: "post 4", upvotes: 4},
 	    {title: "post 5", upvotes: 5}
 	];
+
+	$scope.addPost = function(){
+	    $scope.posts.push({title: 'A new post!', upvotes: 0});
+	};
     }
 ]);
