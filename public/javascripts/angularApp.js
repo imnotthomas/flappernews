ef4158ff07549d54fbbcf2914c4baefa03ece93f
Thomas Altman <thomasjaltman@gmail.com>
2015-09-05 17:30:55 +0000

    adding post factory

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,12 +1,6 @@
-var mainCtrl = function($scope) {
+var mainCtrl = function($scope, posts) {
     $scope.test = 'Hello world!';
-    $scope.posts = [
-	{title: "post 1", upvotes: 1},
-	{title: "post 2", upvotes: 2},
-	{title: "post 3", upvotes: 3},
-	{title: "post 4", upvotes: 4},
-	{title: "post 5", upvotes: 5}
-    ];
+    $scope.posts = posts.posts
 
     $scope.addPost = function(){
 	if(!$scope.title || $scope.title === ''){
@@ -26,10 +20,23 @@ var mainCtrl = function($scope) {
     };
 }    
 
+var postFactory = function(){
+    var o = {
+	posts: [
+	    {title: "post 1", upvotes: 1},
+	    {title: "post 2", upvotes: 2},
+	    {title: "post 3", upvotes: 3},
+	    {title: "post 4", upvotes: 4},
+	    {title: "post 5", upvotes: 5}]
+    };
+    return o;
+};
 
 angular.module('flapperNews', [])
+    .factory('posts',[postFactory])
     .controller('MainCtrl',
 		["$scope",
+		 "posts",
 		 mainCtrl
 		]
     );
