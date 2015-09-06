dfe20de21638b2aac6f03d6f8d0fb29ea03cb112
Thomas Altman <thomasjaltman@gmail.com>
2015-09-06 18:13:23 +0000

    adding upvotes to comments

--- routes/index.js
+++ routes/index.js
@@ -16,9 +16,21 @@ router.param('post', function(req, res, next, id) {
     });
 });
 
+router.param('comment', function(req, res, next, id) {
+    var query = Comment.findById(id);
+
+    query.exec(function(err, comment) {
+	if(err) { return next(err); }
+	if(!comment) {return next(new Error('can\'t find comment'));}
+
+	req.comment = comment;
+	return next()
+    });
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 router.get('/posts', function(req, res, next) {
@@ -41,7 +53,11 @@ router.post('/posts', function(req, res, next) {
 });
 
 router.get('/posts/:post', function(req, res) {
-    res.json(req.post);
+    req.post.populate('comments', function(err, post) {
+	if (err) {return next(err); }
+	
+	res.json(req.post);
+    });
 });
 
 router.put('/posts/:post/upvote', function(req, res, next) {
@@ -49,6 +65,34 @@ router.put('/posts/:post/upvote', function(req, res, next) {
 	if(err) { return next(err); }
 
 	res.json(post);
+    });
+});
+
+router.get('/posts/:post/comments/:comment', function(req, res, next) {
+    res.json(req.comment);
+});
+
+router.post('/posts/:post/comments', function(req, res, next) {
+    var comment = new Comment(req.body);
+    comment.post = req.post;
+
+    comment.save(function(err, comment) {
+	if(err) {return next(err); }
+
+	req.post.comments.push(comment);
+	req.post.save(function(err, post) {
+	    if(err) { return next(err); }
+
+	    res.json(comment);
+	});
+    });
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+    req.comment.upvote(function(err, comment) {
+	if(err) { return next(err); }
+
+	res.json(comment);
     });
 });
 
