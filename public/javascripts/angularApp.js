b7bf38d286474ed9e9ad245cbd119644ee4f6525
Thomas Altman <thomasjaltman@gmail.com>
2015-09-07 13:25:12 +0000

    adding auth factory

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -87,8 +87,60 @@ var postFactory = function($http){
     return o;
 };
 
+var authFactory = function($http, $window){
+  var auth = {};
+
+  auth.saveToken = function(token){
+    $window.localStorage['flapper-news-token'] = token;
+  };
+
+  auth.getToken = function() {
+    return $window.localStorage['flapper-news-token'];
+  };
+
+  auth.isLoggedIn = function(){
+    var token = aut.getToken();
+
+    if(token){
+      var payload = JSON.parse($window.atob(token.split('.'[1])));
+
+      return payload.exp > Date.now() / 1000;
+    } else {
+      return false;
+    }
+  };
+
+  auth.currentUser = function(){
+    if(auth.isLoggedIn()){
+      var token = auth.getToken();
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+      return payload.username;
+    }
+  };
+
+  auth.register = function(user){
+    return $http.post('/register', user).success(function(data){
+      auth.saveToken(data.token);
+    });
+  };
+
+  auth.logIn = function(user){
+    return $http.post('/login', user).success(function(data){
+        auth.saveToken(data.token);
+      });
+  };
+
+  auth.logOut = function(){
+    $window.localStorage.removeItem('flapper-news-token');
+  };
+
+  return auth;
+};
+
 angular.module('flapperNews', ['ui.router'])
     .factory('posts',['$http', postFactory])
+    .factory('auth', ['$http', '$window', authFactory])
     .controller('MainCtrl',
 		["$scope",
 		 "posts",
